6ab9d1012e49ccaf345186c11afb9461ded3cb3a
Jonah Gasura <jonahgasura@gmail.com>
2021-11-16 14:42:08 +0200

    FirstCommit1

---
+++ Main.cpp
@@ -0,0 +1,20 @@
+/*
+	Completed By: 
+		Name: Jonah Gasura
+		Student Number: 20592061
+
+*/
+
+#include "Thruster.h"
+#include "Stage.h"
+#include "StageObserver.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "good - no issues" << endl;
+	return 0;
+}
