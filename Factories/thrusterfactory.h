6b1f6d215cd1329cc2356be6f2285f690d6f4814
Dylan <whiteford957@gmail.com>
2021-11-20 16:24:29 +0200

    added doxygen to thrusterfactory

--- Factories/thrusterfactory.h
+++ Factories/thrusterfactory.h
@@ -10,14 +10,44 @@ using namespace std;
 #include "../merlin.h"
 #include "../vacuum.h"
 
+
+/** 
+ * @brief Thruster Factory
+ * @details Used to create various kinds of composite thrusters
+ */
 class ThrusterFactory
 {
 public:
+
+    /** 
+     * @brief Thruster Constructor
+     */
     ThrusterFactory();
+
+    /** 
+     * @brief Thruster Creator
+     * @details Creates a falcon core thruster
+     * @param[out] Thruster* faclon core composite with 9 merlin engines
+     */
     Thruster* createCoreThruster();
+
+    /** 
+     * @brief Thruster Creator
+     * @details Creates a vaccume thruster
+     * @param[out] Thruster* single merlin vaccume engine with no core
+     */
     Thruster* createVaccumeThruster();
 
+    /** 
+     * @brief Custom Thruster Creator
+     * @details Creates a custom thruster.
+     * @param[out] Thruster* custom composite
+     */
     Thruster* customThrustor(bool hascore, int decorated, int engines);
+
+    /** 
+     * @brief Thruster destructor
+     */
     ~ThrusterFactory();
 };
 
