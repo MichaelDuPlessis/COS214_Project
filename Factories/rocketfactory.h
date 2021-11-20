01156b8135f4d7810f04df6733b8f9be91c9e2bd
Dylan <whiteford957@gmail.com>
2021-11-20 16:10:28 +0200

    added doxygen to rocketfactory

--- Factories/rocketfactory.h
+++ Factories/rocketfactory.h
@@ -14,21 +14,62 @@
 
 using namespace std;
 
+    /** 
+     * @brief Rocket Factory. 
+     * @details Produces Falcon Heavy and Falcon 9
+     */
 class RocketFactory
 {
 
 private:
+    /**  * @brief Holds A stagefactory.  */
     StageFactory* StgFact;
+    /**  * @brief Holds A  dragonfactory.  */
     CapsuleFactory* CFact;
+    /**  * @brief Holds A crewdragonfactory.  */
     CapsuleFactory* CDFact;
 
 public:
+    /** 
+     * @brief Rocket Factory Constructor. 
+     * @param[out] creates all required factories for a rocketfactory
+     */
     RocketFactory();
+
+    /** 
+     * @brief Rocket Factory Constructor. 
+     * @param[in] Factories takes in required factories
+     * @param[out] Factories adds all input factories to a rocketfactory
+     */
     RocketFactory(StageFactory *sf, C_CapsuleFactory *cd, CD_CapsuleFactory *d);
+
+    /** 
+     * @brief Falcon Heavy creator. 
+     * @param[in] s rocket strategy
+     * @param[in] crewdragon bool to determine what kind of capsule is to be added
+     * @param[out] Rocket* returns a FalconHeavy
+     */
     Rocket* buildFalconHeavy(LLStrategy* s, bool crewdragon);
+
+    /** 
+     * @brief Falcon Heavy creator. 
+     * @param[in] s rocket strategy
+     * @param[in] crewdragon bool to determine what kind of capsule is to be added
+     * @param[out] Rocket* returns a Falcon9
+     */
     Rocket* buildFalcon9(LLStrategy* s, bool crewdragon);
 
+
+    /** 
+     * @brief helper function for creating capsules. 
+     * @param[in] crewdragon bool to determine what kind of capsule is to be added
+     * @param[out] Capsule* returns either CrewDragon Capsule or Dragon Capsule
+     */
     Capsule* builldCapsule(bool crewdragon = true);
+
+    /** 
+     * @brief rocket factory destructor. 
+     */
     ~RocketFactory();
 };
 
