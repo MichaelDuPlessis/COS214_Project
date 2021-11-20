8ec5a83606b334f82f56e0718d56ae39670943e1
Dylan <whiteford957@gmail.com>
2021-11-20 16:19:32 +0200

    added doxygen to stagefactory

--- Factories/stagefactory.h
+++ Factories/stagefactory.h
@@ -8,15 +8,42 @@
 #include "thrusterfactory.h"
 
 using namespace std; 
+/** 
+ * @brief Stage Factory
+ * @details Used to create various kinds of stages
+ */
 class StageFactory
 {
 private:
+    /** * @brief Thruster Factory used for adding thrusters to stages.  */
     ThrusterFactory* thrusterFactory ;
 public:
+    /**  * @brief Satellite Factory Constructor.  */
     StageFactory();
+
+
+    /**  
+     *  @brief Satellite Factory Constructor. 
+     *  @param[in] t Thruster factory for creating thrusters
+    */
     StageFactory(ThrusterFactory* t);
+
+    /**  
+     *  @brief stage creator. 
+     *  @param[out] Stage* returns second stage of a rocket(vacuum merlin engine)
+    */
     Stage* createSmallStage();
+
+    /**  
+     *  @brief stage creator. 
+     *  @param[out] Stage* returns first stage of a rocket(either falcon heavy or falcon 9 first stage)
+    */
     Stage* createLargeStage(bool falcon9thruster);
+
+     /**  
+     *  @brief StageFactory Destructor. 
+     *  @details deletes thurster factory attached to itself
+    */
     ~StageFactory();
 
 };
